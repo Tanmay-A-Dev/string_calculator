dd7caf8e25e8e3aee064862cab7ed172cb59983c
Tanmay <tanmayagnihotri75@gmail.com>
2024-04-08 18:25:35 +0530

    Added method defination

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,3 +1,30 @@
 class StringCalculator
+  def add(input)
+    return 0 if input.empty?
 
+    numbers = extract_numbers(input)
+    check_negatives(numbers)
+    numbers.sum
+  end
+
+  private
+
+  def extract_numbers(input)
+    delimiter = extract_delimiter(input)
+    input = input.gsub(/\/\/.\n/, '') # Remove delimiter declaration
+    input.gsub!("\n", ',') # Replace newline characters with comma
+    input.split(delimiter).map(&:to_i)
+  end
+
+  def extract_delimiter(input)
+    match = input.match(/^\/\/(.)\n/)
+    return ',' unless match
+
+    Regexp.escape(match[1])
+  end
+
+  def check_negatives(numbers)
+    negatives = numbers.select { |num| num < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+  end
 end
